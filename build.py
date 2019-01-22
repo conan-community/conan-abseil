6c9eaffc67ae6d9cb9eaa5d71bfe548e9b40f306
Uilian Ries <uilianries@gmail.com>
2019-01-22 17:55:13 -0200

    second commit
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -9,10 +9,12 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     temp_dir = tempfile.mkdtemp()
-    zip_path = os.path.join(temp_dir, "master.zip")
+    target_dir = os.path.join(temp_dir, "abseil-cpp-master")
     print("temp_dir: %s" % temp_dir)
     tools.get("https://github.com/abseil/abseil-cpp/archive/master.zip", destination=temp_dir)
-    shutil.copytree(os.path.join(temp_dir, "abseil-cpp-master"), os.getcwd())
+    files = os.listdir(target_dir)
+    for index in files:
+        shutil.move(os.path.join(target_dir, index), os.getcwd())
 
     #builder = ConanMultiPackager()
     #builder.add_common_builds(shared_option_name="abseil:shared")
