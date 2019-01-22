982a9457b28f93313d73aaece451f588544c92dc
Uilian Ries <uilianries@gmail.com>
2019-01-22 18:29:33 -0200

    Add README
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ README.md
@@ -0,0 +1,39 @@
+# Conan Abseil
+
+[![Download](https://api.bintray.com/packages/conan-community/conan/abseil%3Aconan/images/download.svg)](https://bintray.com/conan-community/conan/abseil%3Aconan/_latestVersion)
+[![Build Status](https://travis-ci.org/conan-community/conan-abseil.svg)](https://travis-ci.org/conan-community/conan-abseil)
+[![Build status](https://ci.appveyor.com/api/projects/status/github/conan-community/conan-abseil?svg=true)](https://ci.appveyor.com/project/ConanCIintegration/conan-abseil)
+
+[Conan.io](https://conan.io) **builder** for [abseil](https://github.com/abseil/abseil-cpp) project.
+
+The packages generated with this *conanfile.py* can be found in [Bintray](https://bintray.com/conan-community/conan/abseil%3Aconan).
+
+## Builder Package
+
+Abseil has Conan supported, however its CI infrastructure is in [review] (https://github.com/abseil/abseil-cpp/pull/197).
+Meanwhile, Conan Community will provide a minimal infrastructure only to build Conan packages.
+This project shall be **DEPRECATED** in the future, when Abseil will provide its own official package.
+
+## Package Version
+
+Since Abseil prefer to adopt "bleeding-edge" flavor, our releases will be created by date (YYYYMMDD).
+The commit id does not help when you want to sort in a package search.
+
+## Basic setup
+
+    $ conan install abseil/20190122@conan/stable
+
+## Project setup
+
+If you handle multiple dependencies in your project is better to add a *conanfile.txt*:
+
+    [requires]
+    abseil/20190122@conan/stable
+
+    [generators]
+    txt
+    cmake
+
+## License
+
+[MIT License](LICENSE)
--- build.py
+++ build.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import tempfile
+import datetime
 import os
 import shutil
 from conans import tools
 from cpt.packager import ConanMultiPackager
 
 
-def download_abseil():
-    print("Downloading abseil project ...")
-    temp_dir = tempfile.mkdtemp()
-    target_dir = os.path.join(temp_dir, "abseil-cpp-master")
-    tools.get("https://github.com/abseil/abseil-cpp/archive/master.zip", destination=temp_dir)
-    files = os.listdir(target_dir)
-    for index in files:
-        shutil.move(os.path.join(target_dir, index), os.getcwd())
+class CPTHelper(object):
+    @property
+    def username(self):
+        return os.getenv("CONAN_USERNAME", "conan")
+
+    @property
+    def channel(self):
+        return os.getenv("CONAN_CHANNEL", "testing")
+
+    @property
+    def version(self):
+        now = datetime.datetime.now()
+        return now.strftime("%Y%m%d")
+
+    @property
+    def reference(self):
+        namespace = "{}/{}".format(self.username, self.channel)
+        return "abseil/{}@{}".format(self.version, namespace)
+
+    def download_abseil(self):
+        print("Downloading abseil project ...")
+        temp_dir = tempfile.mkdtemp()
+        target_dir = os.path.join(temp_dir, "abseil-cpp-master")
+        tools.get("https://github.com/abseil/abseil-cpp/archive/master.zip", destination=temp_dir)
+        files = os.listdir(target_dir)
+        for index in files:
+            shutil.move(os.path.join(target_dir, index), os.getcwd())
 
 
 if __name__ == "__main__":
-    download_abseil()
-    builder = ConanMultiPackager()
+    cpt_helper = CPTHelper()
+    cpt_helper.download_abseil()
+    builder = ConanMultiPackager(
+        reference=cpt_helper.reference,
+        username=cpt_helper.username,
+        channel=cpt_helper.channel)
     builder.add_common_builds(pure_c=False)
     builder.run()
